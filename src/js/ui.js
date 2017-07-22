7df194118192b2de1dc924a7ce13da99eb89a689
pwizla <pwizla+github@gmail.com>
2017-07-22 22:10:39 +0200

    Refactor SASS files and switch to '4-sides' layout

--- src/js/ui.js
+++ src/js/ui.js
@@ -4,28 +4,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var highlight = document.getElementById('myName');
   var rotating = document.getElementById('jobs');
   var hello = document.getElementById('hello');
-  var scientistMargin = document.getElementById('scientistMargin');
-  var devMargin = document.getElementById('devMargin');
-  var designMargin = document.getElementById('designMargin');
-  var writeMargin = document.getElementById('writeMargin');
-  var scientistContent = document.getElementById('scientistContent');
-  var devContent = document.getElementById('devContent');
-  var designContent = document.getElementById('designContent');
-  var writeContent = document.getElementById('writeContent');
+  var titleName = document.getElementById('titleName');
+  var pwLogo = document.getElementById('pwLogo');
+  var sciAnchor = document.getElementById('sciAnchor');
+  var devAnchor = document.getElementById('devAnchor');
+  var desAnchor = document.getElementById('desAnchor');
+  var wriAnchor = document.getElementById('wriAnchor');
+  var descChoose = document.getElementById('choose');
+  var devPanel = document.getElementById('devPanel');
 
   knowMore.onclick = function() {
+    titleName.className = "hidden";
+    pwLogo.className = "header__PW header__PW--visible";
     titleBlock.className = "title__wrapper title__wrapper--up";
     highlight.className = "title--highlight title--highlight-up";
-    knowMore.className = "title__know-more--hidden";
+    knowMore.className = "hidden";
     rotating.className = "title__jobs--hidden";
     hello.className = "title__hello--hidden";
-    scientistMargin.className = "details__scientist-margin";
-    devMargin.className = "details__dev-margin";
-    designMargin.className = "details__design-margin";
-    writeMargin.className = "details__write-margin";
-    scientistContent.className = "details__scientist-content";
-    devContent.className = "details__dev-content";
-    designContent.className = "details__design-content";
-    writeContent.className = "details__write-content";
+    sciAnchor.className = "details__anchor-scientist";
+    devAnchor.className = "details__anchor-dev";
+    desAnchor.className = "details__anchor-design";
+    wriAnchor.className = "details__anchor-write";
+    descChoose.className = "details__descriptor";
+  }
+
+  sciAnchor.onclick = function() {
+    sciPanel.className = "scientist__panel scientist__panel--visible";
+  }
+
+  devAnchor.onclick = function() {
+    devPanel.className = "developer__panel developer__panel--visible";
+  }
+
+  desAnchor.onclick = function() {
+    desPanel.className = "design__panel design__panel--visible";
+  }
+
+  wriAnchor.onclick = function() {
+    wriPanel.className = "write__panel write__panel--visible";
   }
 });
